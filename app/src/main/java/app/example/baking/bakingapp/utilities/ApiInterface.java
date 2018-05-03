d140eeaaeca6b073ccdfff2cc55d9c296e7ad226
lirinna <37187549+lirinna@users.noreply.github.com>
2018-05-03 22:16:17 +0200

    Add ApiInterface

---
+++ app/src/main/java/app/example/baking/bakingapp/utilities/ApiInterface.java
@@ -0,0 +1,17 @@
+package app.example.baking.bakingapp.utilities;
+
+import java.util.List;
+
+import app.example.baking.bakingapp.model.Recipe;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Katy on 02.05.2018.
+ */
+
+public class ApiInterface {
+
+    @GET("59121517_baking/baking.json")
+
+}

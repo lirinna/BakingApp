961d6bce1e9dece8a35df62fbb2c2beff8f9633f
lirinna <37187549+lirinna@users.noreply.github.com>
2018-05-15 11:06:44 +0200

    Add ExoPlayer BasicSteps

--- app/src/main/java/app/example/baking/bakingapp/ui/fragments/StepOverviewFragment.java
+++ app/src/main/java/app/example/baking/bakingapp/ui/fragments/StepOverviewFragment.java
@@ -10,6 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
 import app.example.baking.bakingapp.R;
 import app.example.baking.bakingapp.model.Step;
 
@@ -17,11 +30,12 @@ import app.example.baking.bakingapp.model.Step;
 public class StepOverviewFragment extends Fragment {
 
     private static final String TAG = StepOverviewFragment.class.getSimpleName();
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
 
     public StepOverviewFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -36,25 +50,68 @@ public class StepOverviewFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_step_overview, container, false);
 
-
+        mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.playerView);
 
         Step stepObject = getActivity().getIntent().getParcelableExtra("stepObject");
         if (stepObject != null) {
 
-        String    description =  stepObject.getDescription();
+            String description = stepObject.getDescription();
+            String url = stepObject.getVideoURL();
+
 
             TextView mName = rootView.findViewById(R.id.tv_step_overview_description);
             mName.setText(description);
 
+            initializePlayer(Uri.parse(url));
 
 
-            Log.e(TAG, "description " + description );
+
+            Log.e(TAG, "description " + description);
 
         }
+
+
 
         return rootView;
     }
 
+    /**
+     * Initialize ExoPlayer.
+     * @param mediaUri The URI of the sample to play.
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getContext(), "BakingApp");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
 
 
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+
+    /**
+     * Release the player when the activity is destroyed.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
 }

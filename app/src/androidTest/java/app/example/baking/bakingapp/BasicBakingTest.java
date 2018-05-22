c5adbdbdb7c7401495beee52b5119a822b47bf18
lirinna <37187549+lirinna@users.noreply.github.com>
2018-05-22 15:14:20 +0200

    Add espresso RecyclerView test

--- app/src/androidTest/java/app/example/baking/bakingapp/BasicBakingTest.java
+++ app/src/androidTest/java/app/example/baking/bakingapp/BasicBakingTest.java
@@ -4,6 +4,7 @@ import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.jakewharton.espresso.OkHttp3IdlingResource;
@@ -16,7 +17,10 @@ import app.example.baking.bakingapp.ui.activities.MainActivity;
 import app.example.baking.bakingapp.utils.OkHttpProvider;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 // Source: https://github.com/chiuki/espresso-samples/tree/master/idling-resource-okhttp
@@ -34,7 +38,19 @@ public class BasicBakingTest {
                 "okhttp", OkHttpProvider.getOkHttpInstance());
         IdlingRegistry.getInstance().register(idlingResource);
 
-        onView(withText("Brownies")).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText("Brownies")).check(matches(isDisplayed()));
+
+        IdlingRegistry.getInstance().unregister(idlingResource);
+    }
+
+    @Test
+    public void clickItem() {
+        IdlingResource idlingResource = OkHttp3IdlingResource.create(
+                "okhttp", OkHttpProvider.getOkHttpInstance());
+        IdlingRegistry.getInstance().register(idlingResource);
+
+        onView(withId(R.id.recyclerview_recipe))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
 
         IdlingRegistry.getInstance().unregister(idlingResource);
     }
